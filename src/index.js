ac4838f5afddf3d2a81d7f54892ef7b2273a5437
Alesya Polovets <alesya.polovets@gmail.com>
2018-09-20 15:44:39 +0300

    love-triangle/feature: implemented function getLoveTrianglesCount which calculates how many love triangles phenomenons take place

--- src/index.js
+++ src/index.js
@@ -4,4 +4,16 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+    let count = 0;
+    let a, b, c;
+    preferences.unshift(0);
+    for (let i = 1; i < preferences.length; i++) {
+        a = preferences[i];
+        b = preferences[a];
+        c = preferences[b];
+        if (i == c) {
+            count++;
+        }
+    }
+    return Math.floor(count/3);
 };
